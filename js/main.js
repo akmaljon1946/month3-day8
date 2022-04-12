ad7be54e997bd113802f286cb556eae11ec26176
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-04-12 17:20:07 +0500

    addfirst

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 
-let pin = "pin";
-let u = document.getElementById("demo")
-function generate(pincode) {
+// let pin = "pin";
+// let u = document.getElementById("demo")
+// function generate(pincode) {
   
-  if(pincode === "pin") {
+//   if(pincode === "pin") {
     
-    return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-  }
-  
-    return;
-    
-  }
-
-  let y = (generate(pin));
-  u.innerHTML = y;
-  console.log(y);
-
-
-
-
-
-
-// let e = document.getElementById("kurs")
-
-// function convert(currency,sum) {
-//   if(currency == "sum-rub") {
-
-//     return (sum /143);
-//   }
-//     else if (currency == "usd-sum") {
-
-//     return (sum * 11329);
-  
+//     return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
 //   }
   
-//   return;
-// }
+//     return;
+    
+//   }
 
-// let currency = prompt();
-// let sum = prompt();
+//   let y = (generate(pin));
+//   u.innerHTML = y;
+//   console.log(y);
 
-// let x = (convert(currency,sum));
-// e.innerHTML = "Bugungi kursda :"+ x;
-// console.log(x);
+
+
+
+
+
+let e = document.getElementById("kurs")
+
+function convert(currency,sum) {
+  if(currency == "sum-rub") {
+
+    return (sum /143);
+  }
+    else if (currency == "usd-sum") {
+
+    return (sum * 11329);
+  
+  }
+  
+  return;
+}
+
+let currency = prompt();
+let sum = prompt();
+
+let x = (convert(currency,sum));
+e.innerHTML = "Bugungi kursda :"+ x.toFixed(2);
+console.log(x.toFixed(2));
 
 
 
